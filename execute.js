bc7e915632444e2b74c43ffca48b277da03a58d7
sanghwan <jae6120@naver.com>
2018-06-18 21:59:39 +0900

    promise test

--- execute.js
+++ execute.js
@@ -7,23 +7,43 @@ const main = () => {
     fs.readdir(directory,(err,files)=>{
         if (err) throw err;
         files.forEach((filename) => {
-            if(filename.includes('jpg')||filename.includes('png')) doOptimize(filename);
+            if(filename.includes('jpg')) doOptimize(filename);
         });
     });
 };
 
 const doOptimize = async (filename)=>{
-    let result = {};
-    result.name = filename;
-    result.imageMinJpegOptim = await optimizeService.imageMinJpegOptim(filename,getFilesizeInBytes(filename));
+    let resultObj = {};
+    resultObj.name = filename;
+    const origin_byte = getFilesizeInBytes(`${directory}/${filename}`);
+    try {
+        const start = Date.now();
+        const result = await optimizeService.imageMinJpegOptim1(filename)
+        const millis = Date.now() - start;
+        const compressFileSize = getFilesizeInBytes(result[0].path);
+        const conpressRate = parseInt(compressFileSize*100/origin_byte)+"%";
+        resultObj.imageMinJpegOptim = {rate:conpressRate,time:millis,size:compressFileSize}
+    }
+    catch (err) {
+        console.log('imageMinJpegOptim failed', err);
+    }
+    try {
+        const result = await optimizeService.imageMinJpegtran1(filename,origin_byte)
+        resultObj.imageMinJpegtran = result;
+    }
+    catch (err) {
+        console.log('imageMinJpegtran1 failed', err);
+    }
+    // result.imageMinJpegOptim = await optimizeService.imageMinJpegOptim(filename,getFilesizeInBytes(filename));
     // result.imageMinJpegtran = await optimizeService.imageMinJpegtran(filename,getFilesizeInBytes(filename));
     // result.imageMinMozjpeg = await optimizeService.imageMinMozjpeg(filename,getFilesizeInBytes(filename));
-    console.log(result);
+    console.log(resultObj);
 
 };
 
-const getFilesizeInBytes =(filename)=>{
-    const stats = fs.statSync(`${directory}/${filename}`);
+
+const getFilesizeInBytes =(dir)=>{
+    const stats = fs.statSync(dir);
     const fileSizeInBytes = stats.size;
     return fileSizeInBytes
 };
--- service/optimizeService.js
+++ service/optimizeService.js
@@ -12,7 +12,7 @@ const originDirectory = `${process.cwd()}/sampleFile`;
 
 
 
-Optimize.imageMinJpegOptim = (filename,origin_byte) => {
+Optimize.imageMinJpegOptim = async (filename,origin_byte) => {
     const path = `${originDirectory}/${filename}`;
     const start = Date.now();
     return imagemin([path], `${tempDirectory}/jpegOptim/`, {
@@ -28,7 +28,17 @@ Optimize.imageMinJpegOptim = (filename,origin_byte) => {
     })
 };
 
-Optimize.imageMinMozjpeg = (filename,origin_byte) => {
+Optimize.imageMinJpegOptim1 = (filename) => {
+    const path = `${originDirectory}/${filename}`;
+    return imagemin([path], `${tempDirectory}jpegOptim/`, {
+        plugins: [
+            imageminJpegoptim({max: '80'})
+            , imageminPngquant({quality: '65-80', floyd: 1})
+        ]
+    })
+};
+
+Optimize.imageMinMozjpeg = async (filename,origin_byte) => {
     const path =  `${originDirectory}/${filename}`;
     const start = Date.now();
     return imagemin([path], `${tempDirectory}/Mozjpeg/`, {
@@ -44,7 +54,7 @@ Optimize.imageMinMozjpeg = (filename,origin_byte) => {
     })
 };
 
-Optimize.imageMinJpegtran = (filename,origin_byte) => {
+Optimize.imageMinJpegtran = async (filename,origin_byte) => {
     const path =  `${originDirectory}/${filename}`;
     const start = Date.now();
     return imagemin([path], `${tempDirectory}/Jpegtran/`, {
@@ -58,6 +68,35 @@ Optimize.imageMinJpegtran = (filename,origin_byte) => {
         const conpressRate = parseInt(compressFileSize*100/origin_byte)+"%";
         return {rate:conpressRate,time:millis,size:compressFileSize}
     })
+};
+// Optimize.imageMinJpegtran1 = async (filename) => {
+//     const path =  `${originDirectory}/${filename}`;
+//     return imagemin([path], `${tempDirectory}/Jpegtran/`, {
+//         plugins: [
+//             imageminJpegtran({progressive:true,arithmetic:true})
+//             ,imageminPngquant({quality: '65-80', floyd: 1})
+//         ]
+//     })
+// };
+Optimize.imageMinJpegtran1 = (filename,origin_byte) => {
+    const path =  `${originDirectory}/${filename}`;
+    const start = Date.now();
+    const promise = new Promise(function (resolve, reject) {
+        imagemin([path], `${tempDirectory}/Jpegtran/`, {
+            plugins: [
+                imageminJpegtran({progressive:true,arithmetic:true})
+                ,imageminPngquant({quality: '65-80', floyd: 1})
+            ]
+        }).then((result) => {
+            const millis = Date.now() - start;
+            const compressFileSize = getFilesizeInBytes(result[0].path);
+            const conpressRate = parseInt(compressFileSize*100/origin_byte)+"%";
+            resolve({rate:conpressRate,time:millis,size:compressFileSize})
+        }).catch(function (err) {
+            reject(err)
+        })
+    });
+    return promise
 };
 function getFilesizeInBytes(filename) {
     const stats = fs.statSync(filename);
